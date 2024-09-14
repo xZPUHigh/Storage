5ae8747cee6e0aeb9a7bd55f671535dbc6b0b9fb
ZPU <93166306+xZPUHigh@users.noreply.github.com>
2024-09-14 19:40:49 +0700

    Create code.lua

---
+++ code.lua
@@ -0,0 +1,21 @@
+print("Claim CODES")
+function sendCode(code)
+    local args = {
+        [1] = code
+    }
+    game:GetService("ReplicatedStorage").Networking.CodesEvent:FireServer(unpack(args))
+end
+
+sendCode("RELEASE")
+sendCode("DELAY")
+sendCode("10KLIKES")
+sendCode("100KLIKES")
+sendCode("200KLIKES")
+sendCode("300KLIKES")
+sendCode("400KLIKES")
+sendCode("AV500KLIKES")
+sendCode("600KLIKES")
+sendCode("10MVISITS")
+sendCode("25MVISITS")
+sendCode("AV50MIL")
+sendCode("70MVISITS")
